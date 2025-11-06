0114df5f421c77bf20c0b05cbda4d1425116136c
tsctsc6 <tsctsc6@126.com>
2025-11-06 16:57:55 +0800

    改进状态栏，动态渲染登录登出按钮和用户名称

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
+import { useAuth } from "../contexts/authContext";
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const { user, isLoggedIn, logout } = useAuth();
 
   return (
     <nav className="top-navbar">
       <button onClick={() => navigate("/")}>Home</button>
-      <button onClick={() => navigate("/login")}>Login</button>
       <button onClick={() => navigate("/about")}>About</button>
+      <p className="navbar-user-info">
+        {isLoggedIn ? (
+          <>{`Hello, ${user?.name}`} <button onClick={logout}>Logout</button></>
+        ) : (
+          <button onClick={() => navigate("/login")}>Login</button>
+        )}
+      </p>
     </nav>
   );
 }
