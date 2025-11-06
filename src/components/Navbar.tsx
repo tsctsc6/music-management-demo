12ae107cb6d4dc2822ae069c4f0fd2305b2220ca
tsctsc6 <tsctsc6@126.com>
2025-11-06 14:12:32 +0800

    添加导航栏组件

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+import "./Navbar.css";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="top-navbar">
+      <button onClick={() => navigate("/")}>Home</button>
+      <button onClick={() => navigate("/about")}>About</button>
+    </nav>
+  );
+}
